606df876bdfcfb8824fc9965f9e315bf4c019b85
Magda Kokot <magda.kokot@mandogroup.com>
2022-02-01 15:30:21 +0000

    Create CurrentGame component, add game log data example

---
+++ src/components/CurrentGame.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { defineProps, PropType, computed } from "@vue/runtime-core";
+
+const props = defineProps({
+  games: { type: Object as PropType<any[]> },
+});
+const players = ["Magda", "Ollie"];
+const currentGame = computed(() => props.games[props.games.length - 1]);
+
+const createDate = (timestamp: number) => {
+  return new Date(timestamp).toLocaleString();
+};
+
+// const emit = defineEmits<{ (event: "handleGamesEmit", value: any[]): void }>();
+</script>
+<template>
+  <form class="c-current-game" v-if="games.length">
+    <span class="c-current-game__time">Game {{ createDate(currentGame.time) }}</span>
+    <div
+      class="c-current-game__players"
+      v-for="(playerArmy, i) in currentGame.players"
+      :key="playerArmy"
+    >
+      <div class="c-current-game__player">
+        <span class="c-current-game__name">{{ players[i] }}</span>
+        <span class="c-current-game__army">{{ playerArmy.name }}</span>
+        <input type="number" />
+      </div>
+    </div>
+    <button>SAVE</button>
+  </form>
+</template>
+<style lang="scss">
+.c-army-checkbox__container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+}
+
+.c-army-checkbox__label {
+  display: inline-block;
+  padding: 10px 15px;
+  margin: 5px;
+  border-width: 5px;
+  border-style: solid;
+  cursor: pointer;
+}
+
+.c-army-checkbox__button {
+  padding: 10px 20px;
+  background-color: black;
+  color: white;
+  border-radius: 20px;
+  cursor: pointer;
+  margin-bottom: 20px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,35 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ArmiesForm from "@/components/ArmiesForm.vue";
+import CurrentGame from "@/components/CurrentGame.vue";
+import dataAllArmies from "@/data-all-armies.json";
+
+const armies = ref(dataAllArmies);
+const games = ref([]);
+
+const handleGamesEmit = (payload: any) => {
+  games.value = payload;
+};
+</script>
 <template>
-  <div class="wrapper">
-    <MainPage />
-  </div>
+  <ArmiesForm :armies="armies" @handleGamesEmit="handleGamesEmit" />
+  <CurrentGame :games="games" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import MainPage from "@/components/MainPage.vue"; // @ is an alias to /src
-
-export default defineComponent({
-  name: "Home",
-  components: {
-    MainPage,
-  },
-});
-</script>
-<style lang="scss">
-.wrapper {
-  max-width: 900px;
-  margin: 0 auto;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
