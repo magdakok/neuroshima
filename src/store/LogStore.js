e3671eebd3a4dae4a2b7124a95ba1f7917d7efb0
Magda Kokot <magda.kokot@mandogroup.com>
2022-05-13 12:34:50 +0100

    Pinia - define User and Log stores

---
+++ src/store/LogStore.js
@@ -0,0 +1,4 @@
+import { defineStore } from "pinia";
+
+// FPINIA1: defineStore("UniqueStoreName", { configHere });
+export const useLogStore = defineStore("LogStore", {});
---
+++ src/store/UserStore.js
@@ -0,0 +1,4 @@
+import { defineStore } from "pinia";
+
+// FPINIA1: defineStore("UniqueStoreName", { configHere });
+export const useUserStore = defineStore("UserStore", {});
